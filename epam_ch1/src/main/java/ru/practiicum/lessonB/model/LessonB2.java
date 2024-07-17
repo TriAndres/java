d2683893fca29f6e715e3a3b1e501cd69a7d8cd0
Andrei <An.S-1@yandex.ru>
2024-07-17 11:45:35 +0300

    commit 7

--- epam_ch1/src/main/java/ru/practiicum/lessonB/model/LessonB2.java
+++ epam_ch1/src/main/java/ru/practiicum/lessonB/model/LessonB2.java
@@ -10,5 +10,24 @@ public class LessonB2 extends LessonB {
     @Override
     public void game() {
         System.out.println("2. Наибольшее и наименьшее число.");
+        numAllShow(20, 1, 100, 5);
+        System.out.println("Наибольшее число: " + max());
+        System.out.println("Наименьшее число: " + min());
+    }
+
+    public Integer max() {
+        return number
+                .getNumberList()
+                .stream().map(i-> i.getValue())
+                .max(Integer::compare)
+                .get();
+    }
+
+    public Integer min() {
+        return number
+                .getNumberList()
+                .stream().map(i-> i.getValue())
+                .min(Integer::compare)
+                .get();
     }
 }
