c7f73eb152b940cf7e04175542355c513b7e941b
Andrei <An.S-1@yandex.ru>
2024-07-22 04:02:01 +0300

    commit 13

--- epam_ch1/src/main/java/ru/practiicum/lessonB/model/LessonB10.java
+++ epam_ch1/src/main/java/ru/practiicum/lessonB/model/LessonB10.java
@@ -1,6 +1,10 @@
 package ru.practiicum.lessonB.model;
 
 import ru.practiicum.number.ManageNumber;
+import ru.practiicum.number.Number;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LessonB10 extends LessonB {
     public LessonB10(ManageNumber number) {
@@ -10,5 +14,22 @@ public class LessonB10 extends LessonB {
     @Override
     public void game() {
         System.out.println("10. Числа-палиндромы,значения которых в прямом и обратном порядке совпадают.");
+        numAllShow(20, 100, 999, 5);
+        System.out.println("Числа-палиндромы:");
+        number.show(5,numPalindrome(number.getNumberList()));
+        number.getNumberList().clear();
     }
+
+    private List<Number> numPalindrome(List<Number> line) {
+        List<Number> list = new ArrayList<>();
+        for (Number number1 : number.getNumberList()) {
+            String line1 = String.valueOf(number1.getValue());
+            String line2 = new StringBuilder().append(line1).reverse().toString();
+            if (line1.equals(line2)) {
+                list.add(new Number(number1.getValue()));
+            }
+        }
+        return list;
+    }
+
 }
--- epam_ch1/src/main/java/ru/practiicum/lessonB/model/LessonB11.java
+++ epam_ch1/src/main/java/ru/practiicum/lessonB/model/LessonB11.java
@@ -1,6 +1,10 @@
 package ru.practiicum.lessonB.model;
 
 import ru.practiicum.number.ManageNumber;
+import ru.practiicum.number.Number;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LessonB11 extends LessonB {
     public LessonB11(ManageNumber number) {
@@ -10,5 +14,21 @@ public class LessonB11 extends LessonB {
     @Override
     public void game() {
         System.out.println("11. Элементы, которые равны полусумме соседних элементов.");
+        numAllShow(20, 10, 99, 5);
+        System.out.println("Элементы, которые равны полусумме соседних элементов:");
+        number.show(5,numHalfASum(number.getNumberList()));
+        number.getNumberList().clear();
+    }
+
+    public List<Number> numHalfASum(List<Number> list) {
+        List<Number> numberList = new ArrayList<>();
+        for (int i = 1; i < list.size() - 1; i++) {
+            int num = (list.get(i - 1).getValue() + list.get(i + 1).getValue()) / 2;
+            if (num == list.get(i).getValue()) {
+                System.out.println(number.getNumberList().get(i).getValue() + " ");
+                numberList.add(new Number(list.get(i).getValue()));
+            }
+        }
+        return numberList;
     }
 }
