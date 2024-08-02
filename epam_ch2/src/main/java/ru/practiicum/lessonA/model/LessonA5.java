ac2b959865464117025ded9df5c54782d5297849
Andrei <An.S-1@yandex.ru>
2024-08-02 07:58:40 +0300

    epam_ch2 commit 7

--- epam_ch2/src/main/java/ru/practiicum/lessonA/model/LessonA5.java
+++ epam_ch2/src/main/java/ru/practiicum/lessonA/model/LessonA5.java
@@ -3,6 +3,9 @@ package ru.practiicum.lessonA.model;
 import ru.practiicum.number.model.Number;
 import ru.practiicum.number.model.NumberManage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LessonA5 extends LessonA{
     public LessonA5(NumberManage numberManage) {
         super(numberManage);
@@ -16,8 +19,31 @@ public class LessonA5 extends LessonA{
         System.out.println("Ввод: id-num");
         showNum("0", 10, numberManage.getListNumbers());
 
-        for (Number n : numberManage.getListNumbers()) {
+        System.out.println("Вывод: id-num");
+        showNum("0", 10, num(numberManage.getListNumbers()));
 
+    }
+
+    private List<Number> num(List<Number> list) {
+        List<Number> numberList = new ArrayList<>();
+        for (Number n : numberManage.getListNumbers()) {
+            if (n.getNum() % 2 == 0) {
+                String[] a = String.valueOf(n.getNum()).split("");
+                List<Integer> i1 = new ArrayList<>();
+                List<Integer> i2 = new ArrayList<>();
+                for (String s : a) {
+                    if (Integer.parseInt(s) % 2 == 0) {
+                        i1.add(Integer.parseInt(s));
+                    }
+                    if (Integer.parseInt(s) % 2 != 0) {
+                        i2.add(Integer.parseInt(s));
+                    }
+                }
+                if (i1.size() == i2.size()) {
+                    numberList.add(n);
+                }
+            }
         }
+        return numberList;
     }
 }
